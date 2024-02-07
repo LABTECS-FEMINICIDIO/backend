fd225eeae1559c9d081d4b9db5464a46d1c2cc53
Pedro Soprano <vonhishi@gmail.com>
2024-02-07 13:22:28 -0400

    fix: adicionando id no retorno da criação da vitima

--- src/controller/vitimas_controller.py
+++ src/controller/vitimas_controller.py
@@ -34,7 +34,36 @@ class Vitima(BaseModel):
     sites: Optional[List[UUID]] = []
 
 
-@router.post("/vitimas/", response_model=Vitima)
+class VitimaResponse(BaseModel):
+    id: UUID
+    datadofato: str
+    diah: str
+    horario: str
+    turno: str
+    nome: str
+    idade: int
+    racacor1: str
+    estciv2: str
+    bairro: str
+    rua_beco_travessa_estrada_ramal: str
+    endcomplemento: str
+    tipoarma1: str
+    tipoarma2: str
+    loclesao1: str
+    loclesao2: str
+    loclesao3: str
+    hospitalizacao: str
+    violsexual: str
+    latrocinio: str
+    zona: str
+    localdeocorrencia: str
+    presencafilhofamiliar: str
+    gestacao: str
+    filhosdescrever: int
+    sites: Optional[List[UUID]] = []
+
+
+@router.post("/vitimas/", response_model=VitimaResponse)
 async def create_vitimas_controller(vitima: Vitima):
     return await create_vitima(vitima)
 
