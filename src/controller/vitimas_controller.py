e502cf5bc49c8273941dbb33083d5aab4f4e4e29
Pedro Soprano <vonhishi@gmail.com>
2024-03-05 21:31:27 -0400

    fix: corrigindo rota export iml

--- src/controller/vitimas_controller.py
+++ src/controller/vitimas_controller.py
@@ -105,7 +105,7 @@ async def export_xlsx():
 
         output = export_to_xlsx(vitimas_data)
 
-        return StreamingResponse(content=output, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers={"Content-Disposition": "attachment; filename=export.xlsx"})
+        return StreamingResponse(content=output, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers={"Content-Disposition": "attachment; filename=vitimas.xlsx"})
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
