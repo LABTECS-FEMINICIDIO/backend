b841f1934001151447568312c6fbfb8c807a828f
Pedro Henrique Souza Soprano (BR-ICCT) <pedro_soprano@calcomp-icct.org.br>
2024-04-01 18:07:18 -0400

    filters

--- src/views/sites_view.py
+++ src/views/sites_view.py
@@ -94,13 +94,13 @@ async def list_sites(filters=None):
         SitesModels.vitima_id
     )
     
-    # if filters:
-    #     for key, value in filters.items():
-    #         if key == 'createdAt':
-    #             query = query.filter(SitesModels.createdAt >= value[0], SitesModels.createdAt <= value[1])
-    #         else:
-    #             query = query.filter_by(**{key: value})
-    
+    if filters:
+        for key, value in filters.items():
+            if key == 'createdAt':
+                query = query.filter(SitesModels.createdAt >= value[0], SitesModels.createdAt <= value[1])
+            else:
+                query = query.filter_by(**{key: value})
+    print(filters)
     sites = query.all()
     
     db_session.close()
