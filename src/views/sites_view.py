f39f0717f31f9e76e1bfd92a3f9ab6fdac8a62b1
Pedro Henrique Souza Soprano (BR-ICCT) <pedro_soprano@calcomp-icct.org.br>
2024-04-01 18:17:12 -0400

    fix: filters

--- src/views/sites_view.py
+++ src/views/sites_view.py
@@ -6,7 +6,7 @@ import requests
 from fastapi.encoders import jsonable_encoder
 from psycopg2 import IntegrityError
 from pydantic import BaseModel
-from sqlalchemy import desc
+from sqlalchemy import desc, and_
 from sqlalchemy.orm import sessionmaker, joinedload, selectinload
 from typing import List, Optional, Union
 from src.database.database import engine
@@ -96,11 +96,14 @@ async def list_sites(filters=None):
     
     print(filters)
     if filters:
+        where_clauses = []
         for key, value in filters.items():
-            if key == 'createdAt':
-                query = query.filter(SitesModels.createdAt >= value[0], SitesModels.createdAt <= value[1])
+            if key == 'created_date':
+                where_clauses.append(and_(SitesModels.createdAt >= value[0], SitesModels.createdAt < value[1]))
             else:
-                query = query.filter_by(**{key: value})
+                where_clauses.append(getattr(SitesModels, key) == value)
+        
+        query = query.filter(*where_clauses)
     sites = query.all()
     
     db_session.close()
