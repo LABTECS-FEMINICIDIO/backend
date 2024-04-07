5c3c76a7e92744e691b976632ac6017ac95a7b74
Pedro Soprano <vonhishi@gmail.com>
2024-04-07 11:39:20 -0400

    fix parse iml

--- src/views/sites_view.py
+++ src/views/sites_view.py
@@ -365,8 +365,8 @@ async def parse_excel(file: UploadFile = File(...)):
                         if not is_duplicate_record_for_parse(row):
 
                             await create_iml(Iml(
-                                dataEntrada="NA" if row[0].strip() == "" else datetime.strftime(row[0], '%d/%m/%Y'),
-                                horaEntrada="NA" if row[1].strip() == "" else row[1].strftime('%H:%M'),
+                                dataEntrada= datetime.strftime(row[0], '%d/%m/%Y'),
+                                horaEntrada= row[1].strftime('%H:%M'),
                                 sexo="NA" if row[2].strip() == "" else row[2] ,
                                 idade="NA" if str(row[3]).strip() == "" else str(row[3]) ,
                                 bairroDaRemocao="NA" if row[4].strip() == "" else row[4] ,
