e0a37e69f1394a8c7e0b6ea8c37fd7fa4d7ab53f
Pedro Henrique Souza Soprano (BR-ICCT) <bc4g9466@cal-comp.com.br>
2024-05-17 17:00:16 -0400

    adicionando verificação de tags por pesquisa avançada

--- src/views/sites_view.py
+++ src/views/sites_view.py
@@ -338,35 +338,35 @@ async def find_sites_with_keywords(tempo_agendado):
             tags_encontradas_no_site = await find_tags_on_site(site_info['url'], all_tags)
             check_men_died = await check_words(content) #Se retornar falso é pq achou morte de homem
 
-            if "manaus" in tags_encontradas_no_site:
-                 if len(tags_encontradas_no_site) >= 2:
-                    await create_site(Site(
-                                    nome=site_info['name'],
-                                    link=site_info['url'],
-                                    conteudo=content,
-                                    tagsEncontradas=", ".join(tags_encontradas_no_site)
-                                ), site_info['reference_site_link'])
-            # if content:
-            #     if len(tags_encontradas_no_site) >= 1:
-            #         if site_blocked == True and not check_men_died:
-            #             if "manaus" in tags_encontradas_no_site:
-            #                 await create_site(Site(
-            #                     nome=site_info['name'],
-            #                     link=site_info['url'],
-            #                     conteudo=content,
-            #                     tagsEncontradas=", ".join(tags_encontradas_no_site)
-            #                 ), site_info['reference_site_link'])
-            #                 print('criei o site', site_info['url'])
-            #                 print("------------------------------------------------------")
-            # else:
-            #     await create_site(Site(
-            #         nome=site_info['name'],
-            #         link=site_info['url'],
-            #         conteudo=content,
-            #         tagsEncontradas=", ".join(tags_encontradas_no_site)
-            #         ), site_info['reference_site_link'])
-            #     print('criei o site', site_info['url'])
-            #     print("------------------------------------------------------")
+            # if "manaus" in tags_encontradas_no_site:
+            #      if len(tags_encontradas_no_site) >= 2:
+            #         await create_site(Site(
+            #                         nome=site_info['name'],
+            #                         link=site_info['url'],
+            #                         conteudo=content,
+            #                         tagsEncontradas=", ".join(tags_encontradas_no_site)
+            #                     ), site_info['reference_site_link'])
+            if content:
+                if len(tags_encontradas_no_site) >= 1:
+                    if site_blocked == True and not check_men_died:
+                        if "manaus" in tags_encontradas_no_site:
+                            await create_site(Site(
+                                nome=site_info['name'],
+                                link=site_info['url'],
+                                conteudo=content,
+                                tagsEncontradas=", ".join(tags_encontradas_no_site)
+                            ), site_info['reference_site_link'])
+                            print('criei o site', site_info['url'])
+                            print("------------------------------------------------------")
+            else:
+                await create_site(Site(
+                    nome=site_info['name'],
+                    link=site_info['url'],
+                    conteudo=content,
+                    tagsEncontradas=", ".join(tags_encontradas_no_site)
+                    ), site_info['reference_site_link'])
+                print('criei o site', site_info['url'])
+                print("------------------------------------------------------")
 
 
     return found_sites
