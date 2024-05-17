23592b0a91e3002848f53e2260dce62b19fca834
Pedro Henrique Souza Soprano (BR-ICCT) <bc4g9466@cal-comp.com.br>
2024-05-17 17:08:21 -0400

    feat: só salvando site se conter as tags mandantes

--- src/views/sites_view.py
+++ src/views/sites_view.py
@@ -338,7 +338,7 @@ async def find_sites_with_keywords(tempo_agendado):
             tags_encontradas_no_site = await find_tags_on_site(site_info['url'], all_tags)
             check_men_died = await check_words(content) #Se retornar falso é pq achou morte de homem
 
-            if "manaus" in tags_encontradas_no_site and all_tags[0] in tags_encontradas_no_site and all_tags[1] in tags_encontradas_no_site:
+            if "manaus" in tags_encontradas_no_site and str(all_tags[0]).lower() in tags_encontradas_no_site and str(all_tags[1]).lower() in tags_encontradas_no_site:
                     await create_site(Site(
                                     nome=site_info['name'],
                                     link=site_info['url'],
