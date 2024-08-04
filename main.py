1121bbc12d947775e60d565bf689bbe8ba13e205
Pedro Soprano <vonhishi@gmail.com>
2024-08-04 10:14:20 -0400

    teste

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ app.add_middleware(
 
 scheduler = AsyncIOScheduler()
 
-@scheduler.scheduled_job("cron", day_of_week="*",  hour=10, minute=10)
+@scheduler.scheduled_job("cron", day_of_week="*",  hour=10, minute=20)
 async def execute_daily_task():
     await check_search()
 
