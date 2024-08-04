043ab20607e459f2bc3b9fb5eba3c317c9df8658
Pedro Soprano <vonhishi@gmail.com>
2024-08-04 11:09:06 -0400

    fix: if not exists last search

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ app.add_middleware(
 
 scheduler = AsyncIOScheduler()
 
-@scheduler.scheduled_job("cron", day_of_week="*",  hour=11, minute=5)
+@scheduler.scheduled_job("cron", day_of_week="*",  hour=11, minute=13)
 async def execute_daily_task():
     await check_search()
 
