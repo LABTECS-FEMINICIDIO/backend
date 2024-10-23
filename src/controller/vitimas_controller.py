7975496c9b626b5a1ae17c8dc8ef9cb10e0d11f2
Pedro Soprano <vonhishi@gmail.com>
2024-10-22 21:38:35 -0400

    adicionadno colunar no export do iml

--- src/controller/vitimas_controller.py
+++ src/controller/vitimas_controller.py
@@ -227,7 +227,7 @@ def export_to_xlsx(data):
 
     return output
 
-headers_iml = ["dataEntrada", "horaEntrada", "sexo", "idade", "bairroDaRemocao", "causaMorte", "DataCaptura", "HoraCaptura"]
+headers_iml = ["dataEntrada", "horaEntrada", "sexo", "idade", "bairroDaRemocao", "causaMorte"]
 
 trad_iml = {
     "dataEntrada": "dataEntrada",
@@ -250,19 +250,19 @@ def export_to_xlsx_iml(data):
         row_dict = dict(row)
         row_data = []
 
-        for header in headers_iml:
-            if header == "DataCaptura":
-                created_at = datetime.strptime(row_dict["createdAt"], "%Y-%m-%d %H:%M:%S")
-                value =  created_at.date().strftime("%Y-%m-%d")
-            elif header == "DataCaptura":
-                created_at = datetime.strptime(row_dict["createdAt"], "%Y-%m-%d %H:%M:%S")
-                value =  created_at.time().strftime("%H:%M:%S")
-            else:
-                value = row_dict.get(trad_iml[header], "")
+        # for header in headers_iml:
+        #     if header == "DataCaptura":
+        #         created_at = datetime.strptime(row_dict["createdAt"], "%Y-%m-%d %H:%M:%S")
+        #         value =  created_at.date().strftime("%Y-%m-%d")
+        #     elif header == "DataCaptura":
+        #         created_at = datetime.strptime(row_dict["createdAt"], "%Y-%m-%d %H:%M:%S")
+        #         value =  created_at.time().strftime("%H:%M:%S")
+        #     else:
+        #         value = row_dict.get(trad_iml[header], "")
                 
-            row_data.append(value)
+        #     row_data.append(value)
             
-        # row_data = [row_dict.get(trad_iml[header], "") for header in headers_iml]
+        row_data = [row_dict.get(header, "") for header in headers_iml]
         ws.append(row_data)
 
     output = BytesIO()
