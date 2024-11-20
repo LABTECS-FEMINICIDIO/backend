3f5630858ebba774472b7b9db1f712c164e2f64e
Pedro Soprano <vonhishi@gmail.com>
2024-11-20 19:53:43 -0400

    reduzindo requisições e alterando URL de busca

--- src/views/sites_view.py
+++ src/views/sites_view.py
@@ -355,8 +355,14 @@ async def find_sites_with_keywords(tempo_agendado):
             print("cmbinations", combination)
             print("-------------------------------------------")
             keywords = "+".join(combination)
+            
+            ano = data[0]
+            mes = data[1]
+            dia = data[2]
 
-            search_url = f'https://www.google.com/search?q={keywords}+after%3A{data[0]}%2F{data[1]}%2F{data[2]}'
+            search_url = f'https://www.google.com/search?q={keywords}+&tbs=cdr:1,cd_min:{mes}%2F{dia}%2F{ano},cd_max:11/15/2024'
+            # search_url = f'https://www.google.com/search?q={keywords}+after%3A{data[0]}%2F{data[1]}%2F{data[2]}'
+            # https://www.google.com/search?q=mulher+encontrada+porto+&tbs=cdr:1,cd_min:11/01/2024,cd_max:11/15/2024
             print("url", search_url)
             response = requests.get(search_url)
             print("google respondeu")
@@ -390,16 +396,15 @@ async def find_sites_with_keywords(tempo_agendado):
 
             content = await fetch_content(site_info['url'])
 
-            tags_encontradas_no_site = await find_tags_on_site(site_info['url'], all_tags)
-            check_men_died = await check_words(content) #Se retornar falso é pq achou morte de homem
+            # tags_encontradas_no_site = await find_tags_on_site(site_info['url'], all_tags)
 
-            if "manaus" in tags_encontradas_no_site and str(all_tags[0]).lower() in tags_encontradas_no_site and str(all_tags[1]).lower() in tags_encontradas_no_site:
-                await create_site(Site(
-                                nome=site_info['name'],
-                                link=site_info['url'],
-                                conteudo=content,
-                                tagsEncontradas=", ".join(tags_encontradas_no_site)
-                            ), site_info['reference_site_link'])
+            # if "manaus" in tags_encontradas_no_site and str(all_tags[0]).lower() in tags_encontradas_no_site and str(all_tags[1]).lower() in tags_encontradas_no_site:
+            await create_site(Site(
+                            nome=site_info['name'],
+                            link=site_info['url'],
+                            conteudo=content,
+                            tagsEncontradas=", ".join()
+                        ), site_info['reference_site_link'])
             # if content:
             #     if len(tags_encontradas_no_site) >= 1:
             #         if site_blocked == True and not check_men_died:
